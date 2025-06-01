64ee05e44d048e68f8984b56c43c0aa6513ab4d6
2ShKiPeR <igormokhnatov78@gmail.com>
2025-06-01 15:24:06 +0400

    Init comit

---
+++ src/main/java/com/neymark/LectureLens/LectureLensApplication.java
@@ -0,0 +1,13 @@
+package com.neymark.LectureLens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureLensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neymark/LectureLens/LectureLensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neymark.LectureLens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
