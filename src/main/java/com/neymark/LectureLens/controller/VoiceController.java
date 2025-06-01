905a57e1fda408a30763695c43f742a770894ddf
2ShKiPeR <igormokhnatov78@gmail.com>
2025-06-02 02:53:32 +0400

    Add sentiment analyzer and keywords

---
+++ src/main/java/com/neymark/LectureLens/controller/VoiceController.java
@@ -0,0 +1,37 @@
+package com.neymark.LectureLens.controller;
+
+import com.neymark.LectureLens.service.VoiceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class VoiceController {
+
+    private final VoiceService voiceService;
+
+    @PostMapping("/voice-transcribe")
+    public ResponseEntity<String> transcribe(@RequestParam("audio") MultipartFile audioFile) {
+        String text = voiceService.transcribeWithPython(audioFile);
+        return ResponseEntity.ok(text);
+    }
+
+    @PostMapping("/analyze-sentiment")
+    public ResponseEntity<String> analyzeSentiment(@RequestBody Map<String, String> payload) {
+        String text = payload.get("text");
+        String sentiment = voiceService.analyzeSentiment(text);
+        return ResponseEntity.ok(sentiment);
+    }
+
+    @PostMapping("/extract-keywords")
+    public ResponseEntity<Map<String, Integer>> extractKeywords(@RequestBody Map<String, String> payload) {
+        String text = payload.get("text");
+        Map<String, Integer> keywords = voiceService.extractKeywords(text);
+        return ResponseEntity.ok(keywords);
+    }
+}
---
+++ src/main/java/com/neymark/LectureLens/service/VoiceService.java
@@ -0,0 +1,90 @@
+package com.neymark.LectureLens.service;
+
+import com.neymark.LectureLens.utils.MultipartInputStreamFileResource;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class VoiceService {
+
+    public String transcribeWithPython(MultipartFile file) {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+            MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+            body.add("audio", new MultipartInputStreamFileResource(file.getInputStream(), file.getOriginalFilename()));
+
+            HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+            RestTemplate restTemplate = new RestTemplate();
+            String url = "http://localhost:8001/transcribe";
+
+            ResponseEntity<Map> response = restTemplate.postForEntity(url, requestEntity, Map.class);
+            return response.getBody().get("text").toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "[Ошибка расшифровки]";
+        }
+    }
+
+    public String analyzeSentiment(String text) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String url = "http://localhost:8001/analyze-sentiment";
+
+            Map<String, String> requestBody = new HashMap<>();
+            requestBody.put("text", text);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<Map<String, String>> request = new HttpEntity<>(requestBody, headers);
+
+            ResponseEntity<Map> response = restTemplate.postForEntity(url, request, Map.class);
+            return response.getBody().get("sentiment").toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "[Ошибка анализа тональности]";
+        }
+    }
+
+    public Map<String, Integer> extractKeywords(String text) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String url = "http://localhost:8001/extract-keywords";
+
+            Map<String, String> requestBody = new HashMap<>();
+            requestBody.put("text", text);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<Map<String, String>> request = new HttpEntity<>(requestBody, headers);
+
+            ResponseEntity<Map> response = restTemplate.postForEntity(url, request, Map.class);
+
+            Map<String, Object> rawMap = (Map<String, Object>) response.getBody().get("keywords");
+
+            Map<String, Integer> keywords = new HashMap<>();
+            for (Map.Entry<String, Object> entry : rawMap.entrySet()) {
+                keywords.put(entry.getKey(), ((Number) entry.getValue()).intValue());
+            }
+
+            return keywords;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Map.of("Ошибка", -1);
+        }
+    }
+}
